8e25d629e5be9bf454dbe9832af947ba22c012b3
pragyann007 <pragyanthapaliya027@gmail.com>
2026-03-07 22:19:04 +0545

    fet:db users model done .

--- src/shared/models/Users.js
+++ src/shared/models/Users.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import SecurityUtils from "../utils/Securitutils";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -29,9 +31,83 @@ const userSchema = new mongoose.Schema({
         required:true,
         validate:{
             validator:function(password){
-                
+                if(this.isModified("password") && password && !password.startsWith("$2a$")){
+                    const validations = SecurityUtils.validatePassword(password);
+                    return validations.sucess
 
+                };
+                return true ; 
+
+
+            },
+            message:function (props){
+                if(props.value && !props.value.startsWith("$2a$")){
+                    const validations = SecurityUtils.validatePassword(props.value);
+                    return validations.errors.join("- ");
+                }
+                return "Passwrod validations failed..."
             }
         }
+    },
+    role:{
+        type:String,
+        enum:['super_admin', 'client_admin', 'client_viewer'],
+        default:"client_viewer",
+
+    },
+    clientId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Client",
+        required:function(){
+            return this.role !=="super_admin"
+        }
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    permissions:{
+        canCreateApiKeys: {
+            type: Boolean,
+            default: false,
+        },
+        canManageUsers: {
+            type: Boolean,
+            default: false,
+        },
+        canViewAnalytics: {
+            type: Boolean,
+            default: true,
+        },
+        canExportData: {
+            type: Boolean,
+            default: false,
+        },
+
     }
+},{timestamps:true,collation:"user"})
+
+
+userSchema.pre("save",async function(next){
+    if(!this.modefied("password")){
+        return next();
+    }
+    try {
+
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+        next()
+        
+
+        
+    } catch (error) {
+        next(error)
+        
+    }
+
 })
+
+userSchema.index({clientId:1,isActive:1});
+userSchema.index({role:1});
+
+export const user = mongoose.model("user",userSchema);
